ec802f0475aaef46e65d59befb3b6bb7a07b9d04
Yaroslav Skubenko <yaroslav@example.com>
2026-01-08 17:07:47 +0200

    added github reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
 
-  timeout: 10_000,
+  timeout: 30_000,
   use: {
     baseURL: "https://dummyjson.com",
   },
 
-  reporter: "html",
+  reporter: [["html"], ["github"]],
 });
